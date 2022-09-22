f8b92ac48e4eca72a5ecd5e13264a46a7fb7fade
Qusay <qusay@nadsoft.net>
2022-09-22 17:38:44 +0300

    fix static values

--- src/ui/molecules/DataProductization/ActionHeader/ActionHeaderText.jsx
+++ src/ui/molecules/DataProductization/ActionHeader/ActionHeaderText.jsx
@@ -4,17 +4,22 @@ import { TextAtom } from '../../../atoms/DataProductizationAtoms/ActionHeader/Pa
 import MediaQuery from 'react-responsive';
 import theme from '../../../theme';
 import InfoStatement from './InfoStatement';
+import { useSelector } from 'react-redux';
 
 const ActionHeaderText = () => {
+  const { aov, numberOfVisits, lostRevenueData } = useSelector(
+    ({ DPState }) => DPState,
+  );
+  const { totalLostRevenue } = lostRevenueData || {};
   return (
     <TextAtomsContainer>
       <MediaQuery minWidth={theme.breakpoints.magicMachine}>
         <TextAtom>Companies that see </TextAtom>
-        <TextAtom isBold>1000000</TextAtom>
+        <TextAtom isBold>{numberOfVisits?.label}</TextAtom>
         <TextAtom> monthly visitors and have an AOV of </TextAtom>
-        <TextAtom isBold>$150</TextAtom>
+        <TextAtom isBold>{aov?.label}</TextAtom>
         <TextAtom> lose – on average –</TextAtom>
-        <TextAtom isBold>$500000</TextAtom>
+        <TextAtom isBold> {totalLostRevenue} </TextAtom>
         <TextAtom>
           in revenue every month.
           <br /> Do you want a
